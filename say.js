6d43dca2ea637f38387fde2292000c35a4cd9207
zoeying <296852455@qq.com>
2018-05-05 14:03:44 +0800

    升级say

--- say.js
+++ say.js
@@ -11,26 +11,38 @@ if(!touchSensor.connected) {
     process.exit(1);
 }
 
+console.log("Try to press the touch sensor ...");
+
 // 监听触摸传感器
 setInterval(function() {
     var touchValue =  touchSensor.getValue(0);
-    console.log( touchValue )
     if ( touchValue == 1 ){
-        exec( "sh ./saySomethingWithESpeak.sh", 
-            { encoding: 'utf8' } ,
-            function( err , stdout , stderr ){
-        
-            // 如果出错
-            if(err) {
-                console.log('exec err:'+stderr);
-            } else {
-        
-                console.log(stdout);
-        
-            }
-        });
+        console.log("Here we go!");
+        saysomething();
     }
 }, 10);
+
+
+function saysomething() {
+    doExec( "fortune" , function( words ){
+        console.log( words );
+        // display or print the words
+        doExec( `espeak "${ words }" -s 100 --stdout | aplay` )
+
+    })
+}
+
+
+function doExec( command , callback ) {
+    exec( command , { encoding:'utf8'}, function( err , stdout , stder ){
+        // 如果出错
+        if(err) {
+            console.log('exec err:'+stderr);
+        } else {
+            callback && callback( stdout )
+        }
+    }) 
+}
 
 // 执行指令 
 // https://nodejs.org/docs/latest-v0.10.x/api/child_process.html#child_process_child_process_exec_command_options_callback
